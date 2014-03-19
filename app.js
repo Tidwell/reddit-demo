44b70989e8d32e06c3fbe73aebc286afd5c9295a
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-03-19 13:59:47 -0400

    fixing comments

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var app = express();
 //tell express that we want to be able to parse form data
 app.use(express.urlencoded());
 
-//set up a route to serve our test.html when the user goes to localhost:8080/test
+//set up a route to serve index.html when the user goes to localhost:8080
 app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/views/index.html');
 });
 
-//when the user submits a form to localhost:8080/test we just echo back the data they sent
+//when the user submits a form to localhost:8080/test we serve test.html
 app.post('/test', function(req, res) {
 	res.sendfile(__dirname + '/views/test.html');
 });
