eee590bb9cc348790da18e858f21117d8c5c9b70
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-03-19 13:57:51 -0400

    changing routing

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var app = express();
 app.use(express.urlencoded());
 
 //set up a route to serve our test.html when the user goes to localhost:8080/test
-app.get('/test', function(req, res) {
-	res.sendfile(__dirname + '/views/test.html');
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/views/index.html');
 });
 
 //when the user submits a form to localhost:8080/test we just echo back the data they sent
 app.post('/test', function(req, res) {
-	res.end('Thank you, '+req.body.email);
+	res.sendfile(__dirname + '/views/test.html');
 });
 
 //bind the server to port 8080
